141579b0c5a994de5d85889b9af40cc6cce039eb
bernat <bernat.grifol@enti.cat>
2019-02-23 11:33:29 +0100

    v0.3

---
+++ Melody_0/Assets/Scripts/player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+    public float vel;
+
+    public float jump;
+
+    private Rigidbody2D rb;
+    private CapsuleCollider2D cc;
+	// Use this for initialization
+	void Start () {
+
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        cc = gameObject.GetComponent<CapsuleCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-vel, rb.velocity.y);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(vel, rb.velocity.y);
+        }
+
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jump);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            cc.enabled = false;
+        }
+        else
+        {
+            cc.enabled = true;
+        }
+
+	}
+}
