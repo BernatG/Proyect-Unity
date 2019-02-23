b48bd7cf22c54a2a17f457e0cca680c29342cf1f
bernat <bernat.grifol@enti.cat>
2019-02-23 15:58:41 +0100

    v0.3.3

--- Melody_0/Assets/Scripts/player.cs
+++ Melody_0/Assets/Scripts/player.cs
@@ -4,40 +4,43 @@ using UnityEngine;
 
 public class player : MonoBehaviour {
 
-    public float vel;
 
-    public float jump;
 
+    public float speed = 25f;
+    public float maxSpeed = 5f;
+    public bool groudead;
+    public float jumpPower = 6.5f;
+
+    private Rigidbody2D rb2d;
     private Rigidbody2D rb;
     private CapsuleCollider2D cc;
-	// Use this for initialization
-	void Start () {
+    private bool jump;
+
+    // Use this for initialization
+
+    //void CollisionEnter2D(Collision2D colisión)
+    //{
+    //    if (colisión.gameObject.tag == "llave")
+    //    {
+    //        Destroy(colisión.gameObject);
+    //    }
+    //}
+
+    void Start () {
 
         rb = gameObject.GetComponent<Rigidbody2D>();
         cc = gameObject.GetComponent<CapsuleCollider2D>();
-	}
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+
+    }
 	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+	void Update ()
+    {
 
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.Space) && groudead)
         {
-            rb.velocity = new Vector2(-vel, rb.velocity.y);
-        }
-
-        else if (Input.GetKey(KeyCode.D))
-        {
-            rb.velocity = new Vector2(vel, rb.velocity.y);
-        }
-
-        else
-        {
-            rb.velocity = new Vector2(0, rb.velocity.y);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jump);
+            jump = true;
         }
 
         if (Input.GetKey(KeyCode.S))
@@ -50,4 +53,25 @@ public class player : MonoBehaviour {
         }
 
 	}
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        rb2d.AddForce(Vector2.right * speed * h);
+
+        if (rb2d.velocity.x > maxSpeed)
+        {
+            rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
+        }
+        if (rb2d.velocity.x < -maxSpeed)
+        {
+            rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);
+        }
+        if (jump)
+        {
+            rb2d.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            jump = false;
+        }
+        Debug.Log(rb2d.velocity.x);
+    }
 }
