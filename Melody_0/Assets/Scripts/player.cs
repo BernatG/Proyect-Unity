30de140596e178b235a993eb54ecfd62aaa4effd
Walter <walterscherkfontanals@gmail.com>
2019-02-28 22:11:24 +0100

    v.3.4

--- Melody_0/Assets/Scripts/player.cs
+++ Melody_0/Assets/Scripts/player.cs
@@ -8,9 +8,14 @@ public class player : MonoBehaviour {
 
     public float vel;
     public float run;
-    public bool grouded;
+    private bool grouded;
+    private bool doublejump;
+    private bool jump2;
     public float jumpPower = 6.5f;
+    private bool shot;
 
+    private GameObject instantiatedProjectile;
+    public GameObject projectile;
     private Rigidbody2D rb;
     private CapsuleCollider2D cc;
     //private bool jump;
@@ -22,16 +27,19 @@ public class player : MonoBehaviour {
         rb = gameObject.GetComponent<Rigidbody2D>();
         cc = gameObject.GetComponent<CapsuleCollider2D>();
         run = vel + 3;
- 
+        shot = true;
+        doublejump = false;
     }
 	
     // Update is called once per frame
 	void Update ()
     {
-        
+        //Debug.Log(rb.transform.localScale);
         if (Input.GetKey(KeyCode.A))
         {
-             rb.velocity = new Vector2(-vel, rb.velocity.y);
+            rb.velocity = new Vector2(-vel, rb.velocity.y);
+            rb.transform.localScale = new Vector3(3.3075f, rb.transform.localScale.y, rb.transform.localScale.z);
+
             if (Input.GetKey(KeyCode.LeftShift))
             {
                 rb.velocity = new Vector2(-run, rb.velocity.y);
@@ -41,20 +49,17 @@ public class player : MonoBehaviour {
         else if (Input.GetKey(KeyCode.D))
         {
             rb.velocity = new Vector2(vel, rb.velocity.y);
+            rb.transform.localScale = new Vector3(-3.3075f, rb.transform.localScale.y, rb.transform.localScale.z);
             if (Input.GetKey(KeyCode.LeftShift))
             {
                 rb.velocity = new Vector2(run, rb.velocity.y);
+                
             }
         }
         else
         {
             rb.velocity = new Vector2(0, rb.velocity.y);
         }
-
-        /*if (Input.GetKeyDown(KeyCode.Space) && groudead)
-        {
-            jump = true;
-        }*/
 
         if (Input.GetKey(KeyCode.S))
         {
@@ -70,21 +75,30 @@ public class player : MonoBehaviour {
                 rb.velocity = new Vector2(rb.velocity.x, jumpPower);
                 grouded = false;
             }
+            else if (jump2 == true && doublejump == true) {
+                rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+                jump2 = false;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightControl) /*&& shot == true*/) {
+
+            /*instantiatedProjectile = Instantiate(projectile, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z), Quaternion.Euler(0, 0, 0), gameObject.transform);
+            if (rb.transform.localScale.x == -3.3075f) {
+                instantiatedProjectile.GetComponent<proyectil_jugador>().vel *= (-1); 
+                //instantiatedProjectile.GetComponent<Rigidbody2D>().velocity = new Vector2(vel, 0);
+            }
+            //else
+                //instantiatedProjectile.GetComponent<Rigidbody2D>().velocity = new Vector2(-vel, 0);
+            */
         }
 
 	}
-    /*void FixedUpdate()
-    {
-        if (jump)
-        {
-            rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
-            jump = false;
-        }
-        Debug.Log(rb.velocity.x);
-    }*/
+
     private void OnCollisionStay2D(Collision2D collision)
     {
         grouded = true;
+        jump2 = true;
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
--- Melody_0/Assets/Scripts/proyectil_jugador.cs
+++ Melody_0/Assets/Scripts/proyectil_jugador.cs
@@ -2,31 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class proyectil : MonoBehaviour {
+public class proyectil_jugador : MonoBehaviour {
 
     public float timeLimit;
     public float vel;
 
     private Rigidbody2D rb;
     private float time;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         rb = gameObject.GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (rb.transform.rotation.y == 0) {
-            rb.velocity = new Vector2(-vel, 0);
-        }
-        else
-            rb.velocity = new Vector2(vel, 0);
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(vel, 0);
 
         time += Time.deltaTime;
-        if (time >= timeLimit) {
+        if (time >= timeLimit)
+        {
             Destroy(this.gameObject);
             time = 0;
         }
-	}
+    }
 }
