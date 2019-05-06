55dba622b9287f21a9b85aad7e114cd16ca6ffe4
Bernat <bernat.grifol@enti.cat>
2019-05-06 23:01:57 +0200

    Menu y musica

--- Melody_0/Assets/Scripts/menu.cs
+++ Melody_0/Assets/Scripts/menu.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class menu : MonoBehaviour
 {
 
+    public Canvas menus;
+    public Canvas controls;
+
+    void Start()
+    {
+        menus.gameObject.SetActive(true);
+        controls.gameObject.SetActive(false);
+    }
+
     public void changemenuscene(string scenename)
     {
         Application.LoadLevel(scenename);
@@ -13,5 +22,17 @@ public class menu : MonoBehaviour
     public void exitGame()
     {
         Application.Quit();
+    }
+
+    public void Controls()
+    {
+        menus.gameObject.SetActive(false);
+        controls.gameObject.SetActive(true);
+    }
+
+    public void Back()
+    {
+        menus.gameObject.SetActive(true);
+        controls.gameObject.SetActive(false);
     }
 }
