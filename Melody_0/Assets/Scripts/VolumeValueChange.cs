e07479f504a55974b5311e02042cd8178ce7550d
Bernat <bernat.grifol@enti.cat>
2019-05-06 14:37:13 +0200

    musica

---
+++ Melody_0/Assets/Scripts/VolumeValueChange.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeValueChange : MonoBehaviour
+{
+
+    public  AudioMixerGroup musica;
+    private float musicVolume = 1f;
+
+  
+    void Start()
+    {
+        musica = GetComponent<AudioMixerGroup>();
+    }
+
+    void Update()
+    {
+
+        musica = musicVolume;
+    }
+
+    public void SetVolume(float vol)
+    {
+        musicVolume = vol;
+    }
+}
---
+++ Melody_0/Assets/Scripts/musica_menu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class musica_menu : MonoBehaviour {
+
+    private AudioSource audioSource;
+    public AudioClip clip;
+
+    // Use this for initialization
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = clip;
+        audioSource.Play();
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "Level1")
+        {
+            audioSource.Stop();
+        }
+    }
+}
