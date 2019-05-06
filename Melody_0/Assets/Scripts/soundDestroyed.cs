85c4fb68e3a353d7aeaa6ec3faa4070943858aff
Walter <walterscherkfontanals@gmail.com>
2019-05-06 14:33:43 +0200

    Sonido

---
+++ Melody_0/Assets/Scripts/soundDestroyed.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundDestroyed : MonoBehaviour {
+
+    public GameObject enemy;
+    public AudioSource audio;
+	// Use this for initialization
+	void Start () {
+        audio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (enemy.activeInHierarchy == false)
+        {
+            audio.Play();
+        }
+	}
+}
