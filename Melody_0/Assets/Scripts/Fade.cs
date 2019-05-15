39b1f1cd024f7b60573071f537f993cb124c7850
Walter <walterscherkfontanals@gmail.com>
2019-05-15 11:31:18 +0200

    v.7.3
    Fade y decoraciones de fondo

---
+++ Melody_0/Assets/Scripts/Fade.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade : MonoBehaviour {
+
+    //public bool _in = false;
+
+	// Use this for initialization
+	void Start () {
+
+        GetComponent<Animator>().SetTrigger("fade");
+        GetComponent<Animator>().SetBool("fade_out", true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
