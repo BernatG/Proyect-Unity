af37c1859c28249b9880240fcbe759f7009954a4
Bernat <bernat.grifol@enti.cat>
2019-05-17 12:30:55 +0200

    logo

---
+++ Melody_0/Assets/Scripts/logo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class logo : MonoBehaviour {
+
+    int contador;
+
+	// Use this for initialization
+	void Start () {
+        contador = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        contador += 1;
+        if (contador == 232)
+        {
+            Application.LoadLevel("Menu");
+        }
+	}
+}
